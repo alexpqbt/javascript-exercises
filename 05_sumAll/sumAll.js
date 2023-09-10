9092cc8d59cd453f34efb0229c6f04757e3e4e58
Andrei Fox <115098095+alexpqbt@users.noreply.github.com>
2023-09-10 12:20:09 +0800

    Solve exercise 05 with Gauss summation formula

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+  if (!Number.isInteger(min) || !Number.isInteger(max) || min < 0 || max < 0) return "ERROR";
+  else if (min > max) [min, max] = [max, min];
+  return ((max - min) + 1) * (min + max) / 2;
 };
 
 // Do not edit below this line
